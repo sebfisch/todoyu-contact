a5811818aca8459ef162841949ac513fbf71d78f
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-10-21 12:59:08 +0000

    * Records editing cleanup

--- config/admin.php
+++ config/admin.php
@@ -7,7 +7,7 @@
 *
 *  This script is part of the todoyu project.
 *  The todoyu project is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License, version 2, 
+*  it under the terms of the GNU General Public License, version 2,
 *  (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) as published by
 *  the Free Software Foundation;
 *
@@ -21,19 +21,19 @@
 
 TodoyuExtManager::addRecordConfig('contact', 'jobtype', array(
 	'label'		=> 'LLL:contact.contacttypes.jobtype',
-	'list'		=> 'TodoyuContactManager::ListJobtypes',
+	'list'		=> 'TodoyuJobtypeManager::getRecords',
 	'form'		=> 'ext/contact/config/form/admin/jobtype.xml',
 	'object'	=> 'TodoyuJobType',
-	'delete'	=> 'TodoyuJobtypeManager::removeJobType',
+	'delete'	=> 'TodoyuJobtypeManager::deleteJobtype',
 	'save'		=> 'TodoyuJobtypeManager::saveJobType',
 ));
 
 TodoyuExtManager::addRecordConfig('contact', 'customerrole', array(
 	'label'		=> 'LLL:contact.contacttypes.customerrole',
-	'list'		=> 'TodoyuContactManager::ListCustomerroles',
+	'list'		=> 'TodoyuCustomerRoleManager::getRecords',
 	'form'		=> 'ext/contact/config/form/admin/jobtype.xml',
 	'object'	=> 'TodoyuCustomerRole',
-	'delete'	=> 'TodoyuCustomerRoleManager::removeCustomerRole',
+	'delete'	=> 'TodoyuCustomerRoleManager::deleteCustomerRole',
 	'save'		=> 'TodoyuCustomerRoleManager::saveCustomerRole',
 ));
 ?>
