7f758670b1ca9ea5bd442420a1fcd7ecfdeb4457
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-11-06 12:47:19 +0000

    * minor JS cleanup and commenting

--- assets/js/Person.js
+++ assets/js/Person.js
@@ -1,14 +1,43 @@
-/**
- * @author ferni
- */
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 snowflake productions gmbh
+*  All rights reserved
+*
+*  This script is part of the todoyu project.
+*  The todoyu project is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License, version 2,
+*  (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) as published by
+*  the Free Software Foundation;
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
 Todoyu.Ext.contact.Person =  {
 
 	ext: Todoyu.Ext.contact,
 
+	
+	
+	/**
+	 *	Add person (create and edit new person record) 
+	 */
 	add: function() {
 		this.edit(0);
 	},
 
+	
+	
+	/**
+	 *	Edit (person)
+	 *
+	 *	@paran	Integer		idPerson
+	 */
 	edit: function(idPerson) {
 		var url = Todoyu.getUrl('contact', 'person');
 		var options = {
@@ -22,10 +51,25 @@ Todoyu.Ext.contact.Person =  {
 		this.ext.updateContent(url, options);
 	},
 
+	
+	
+	/**
+	 *	On edit (person) handler
+	 *
+	 *	@paran	Integer		idPerson
+	 *	@paran	Unknown		response
+	 */
 	onEdit: function(idPerson, response) {
 		this.observeFieldsForShortname(idPerson);
 	},
 
+	
+	
+	/**
+	 *	Remove person
+	 *
+	 *	@paran	Integer		idPerson
+	 */
 	remove: function(idPerson) {
 		if( confirm('[LLL:contact.person.remove.confirm]') )	{
 			var url = Todoyu.getUrl('contact', 'person');
@@ -41,16 +85,38 @@ Todoyu.Ext.contact.Person =  {
 		}
 	},
 
+	
+	
+	/**
+	 *	On removed handler
+	 *
+	 *	@paran	Integer		idPerson
+	 *	@paran	Unknown		response
+	 */
 	onRemoved: function(idPerson, response) {
 		this.showList();
 	},
 
 
+	
+	/**
+	 *	Observe fields for short name
+	 *
+	 *	@paran	Integer		idPerson
+	 */
 	observeFieldsForShortname: function(idPerson) {
 		$('person-' + idPerson + '-field-lastname').observe('keyup', this.generateShortName.bindAsEventListener(this, idPerson));
 		$('person-' + idPerson + '-field-firstname').observe('keyup', this.generateShortName.bindAsEventListener(this, idPerson));
 	},
 
+	
+	
+	/**
+	 *	Generate short name
+	 *
+	 *	@paran	unknown	event
+	 *	@paran	Integer	idPerson
+	 */
 	generateShortName: function(event, idPerson) {
 		var lastname	= $F('person-' + idPerson + '-field-lastname');
 		var firstname	= $F('person-' + idPerson + '-field-firstname');
@@ -62,6 +128,11 @@ Todoyu.Ext.contact.Person =  {
 
 
 
+	/**
+	 *	Save form
+	 *
+	 *	@paran	String	form
+	 */
 	save: function(form) {
 		$(form).request ({
 				'parameters': {
@@ -73,6 +144,13 @@ Todoyu.Ext.contact.Person =  {
 		return false;
 	},
 
+	
+	
+	/**
+	 *	On saved handle
+	 *
+	 *	@paran	Array	response
+	 */
 	onSaved: function(response) {
 		var error	= response.hasTodoyuError();
 
@@ -85,6 +163,13 @@ Todoyu.Ext.contact.Person =  {
 		}
 	},
 
+	
+	
+	/**
+	 *	Show list
+	 *
+	 *	@paran	String	sword
+	 */
 	showList: function(sword) {
 		var url = Todoyu.getUrl('contact', 'person');
 		var options = {
@@ -97,6 +182,13 @@ Todoyu.Ext.contact.Person =  {
 		this.ext.updateContent(url, options);
 	},
 
+	
+	
+	/**
+	 *	Edit user record
+	 *
+	 *	@paran	Integer	idUser
+	 */
 	editUserRecord: function(idUser) {
 		var params	= {
 			'ext': 'admin',
@@ -109,6 +201,12 @@ Todoyu.Ext.contact.Person =  {
 	},
 
 
+	
+	/**
+	 *	Show person
+	 *
+	 *	@paran	Integer	idPerson
+	 */
 	show: function(idPerson) {
 		var url		= Todoyu.getUrl('contact', 'person')
 		var options	= {
