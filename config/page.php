269dd4396c25b32864021ba35ac14959d77d6527
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-12-07 12:24:42 +0000

    * Fixes

--- config/page.php
+++ config/page.php
@@ -3,8 +3,8 @@
 	// Add menu entries
 if( TodoyuAuth::isLoggedIn() && allowed('contact', 'use') ) {
 	TodoyuFrontend::addMenuEntry('contact', 'LLL:contact.page.title', '?ext=contact', 100);
-	TodoyuFrontend::addSubmenuEntry('contact', 'contact', 'LLL:contact.subMenuEntry.person', '?ext=contact&type=person', 105, 'person');
-	TodoyuFrontend::addSubmenuEntry('contact', 'contact', 'LLL:contact.subMenuEntry.company', '?ext=contact&type=company', 110, 'company');
+	TodoyuFrontend::addSubmenuEntry('contact', 'contactPerson', 'LLL:contact.subMenuEntry.person', '?ext=contact&type=person', 105);
+	TodoyuFrontend::addSubmenuEntry('contact', 'contactCompany', 'LLL:contact.subMenuEntry.company', '?ext=contact&type=company', 110);
 }
 
 ?>
